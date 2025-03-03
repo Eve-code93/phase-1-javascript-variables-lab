88ca5b05d0a58b1570e1b2b386afba0271d00db4
Eve-code93 <evelynejose1993@gmail.com>
2025-03-03 18:20:14 +0300

    Added index.js with company details

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+const companyName = 'Scuber';  // Defined as a constant
+
+let mostProfitableNeighborhood = 'Chelsea';  // Defined using let
+
+let companyCeo = 'Susan Smith';  // Defined using let
